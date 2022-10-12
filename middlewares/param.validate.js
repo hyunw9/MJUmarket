7fb125a4c5d44568212d85662db4a7a932a5e236
강현욱 <rkdgusdnr32@naver.com>
2022-10-12 19:56:12 +0900

    feat : register Controller

--- middlewares/param.validate.js
+++ middlewares/param.validate.js
@@ -1,4 +1,4 @@
-/*import { validationResult } from "express-validator/src/validation-result";
+import { validationResult } from "express-validator/src/validation-result";
 import httpStatus from "http-status";
 
 const validate = (req, res, next) => {
@@ -12,4 +12,3 @@ const validate = (req, res, next) => {
 };
 //유효성검증
 export default validate;
-*/
