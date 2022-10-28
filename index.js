6c55ee6520556b519fc0411f5f0de95b16cba91a
강현욱 <rkdgusdnr32@naver.com>
2022-10-29 02:49:05 +0900

    feat: publish

--- index.js
+++ index.js
@@ -6,7 +6,15 @@ const app = express();
 const PORT = 8080;
 
 app.use(express.json());
-
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "http://localhost:8080",
+      "ec2-3-38-210-186.ap-northeast-2.compute.amazonaws.com",
+    ],
+  })
+);
 app.use(globalRouter);
 //lowdb 선언
 
@@ -14,6 +22,7 @@ app.use((err, req, res, next) => {
   console.log(err);
   res.status(err.statusCode || 500).send(err);
 });
+
 app.get("/test", (req, res) => {
   res.json("ok");
 });
