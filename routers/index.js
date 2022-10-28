1e54b2ddc64261733f14a78884da8c40e131c0c6
강현욱 <rkdgusdnr32@naver.com>
2022-10-29 02:16:56 +0900

    feat: publish

--- routers/index.js
+++ routers/index.js
@@ -6,4 +6,7 @@ const router = Router();
 router.use("/user", authRouter);
 router.use("/post", postRouter);
 router.use("/auth", authRouter);
+router.use("/", async (req, res) => {
+  res.send("Hello");
+});
 export default router;
